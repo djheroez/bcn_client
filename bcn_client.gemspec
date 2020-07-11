425442fe1c14ee73c17253d3c7d9e8b7548eb56d
Dennis Hernandez <djheroez@gmail.com>
2020-07-11 10:43:16 -0600

    Rubocop

--- bcn_client.gemspec
+++ bcn_client.gemspec
@@ -1,4 +1,3 @@
-
 lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "bcn_client/version"
@@ -32,9 +31,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_runtime_dependency "savon", "= 2.12.0"
+  spec.add_runtime_dependency "savon", "~> 2.12"
   spec.add_dependency "nokogiri", "~> 1.10"
   spec.add_development_dependency "bundler", "~> 2.1"
   spec.add_development_dependency "rake", "~> 13.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "rubocop", "~> 0.87"
 end
--- lib/bcn_client/day_rate.rb
+++ lib/bcn_client/day_rate.rb
@@ -1,27 +1,21 @@
-require 'bigdecimal'
-
 module BcnClient
   class DayRate
     attr_accessor :day, :month, :year, :rate, :date, :value
 
     def initialize(bcn_rate = nil)
-      if(!bcn_rate.nil?)
-        @date = bcn_rate[:fecha],
-        @year = bcn_rate[:ano]
-        @month = bcn_rate[:mes]
-        @day = bcn_rate[:dia]
-        @value = BigDecimal(bcn_rate[:valor])
-      end
+      return if bcn_rate.nil?
+
+      @date = bcn_rate[:fecha]
+      @year = bcn_rate[:ano]
+      @month = bcn_rate[:mes]
+      @day = bcn_rate[:dia]
+      @value = BigDecimal(bcn_rate[:valor])
     end
 
     def inspect
-      "DayRate { " +
-        "date: #{@date}, " +
-        "year: #{@year}, " +
-        "month: #{@month}, " +
-        "day: #{@day}, " +
-        "value: " + @value.to_s('F') +
-      "}"
+      "#{self.class} " \
+      "{ date: #{@date}, year: #{@year}, month: #{@month}, day: #{@day}," \
+      "value: #{(@value&.to_s('F') || '')} }"
     end
   end
 end
